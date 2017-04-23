fcc96104f485fa134a91ce2bd1558fb88026210d
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-23 15:18:09 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  // add a constructor for state using 'reactstate'
+  //static propTypes = {
+  //  stringProp: PropTypes.string.isRequired,
+  //  arrayProp: PropTypes.array.isRequired, 
+  //  funcProp: PropTypes.func.isRequired
+  //};
+  
+  render() {
+    return (
+      <div>Countdown Champ, App component</div>
+    );
+  }
+}
+
+//App.defaultProps = {
+  //
+//};
+
+// for docs on prop type validations type `reactvalidateproptypedocs`
+
+module.exports = App;
