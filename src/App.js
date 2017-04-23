24ce1e754f95c920794c9bf8111a40d55530a515
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-23 15:38:04 -0700

    adds countdown timer with basic styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,19 @@ class App extends Component {
   
   render() {
     return (
-      <div>Countdown Champ, App component</div>
+      <div className="tc mt6">
+        <div id="App-title" className="f2">Countdown to December 25, 2017</div>
+        <div>
+          <span className="mh1" id="Clock-days">14 days</span>
+          <span className="mh1" id="Clock-hours">30 hours</span>
+          <span className="mh1" id="Clock-minutes">15 minutes</span>
+          <span className="mh1" id="Clock-seconds">20 seconds</span>
+        </div>
+        <div className="mt2">
+          <input type="text" placeholder='new date' /> 
+          <button>Submit</button>
+        </div>
+      </div>
     );
   }
 }
