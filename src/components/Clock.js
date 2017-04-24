31905ddbdfaa6594542d68b4a71ef11d1de0b41a
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-24 16:24:25 -0700

    adds working counter to clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -13,6 +13,23 @@ class Clock extends Component {
   static propTypes = {
     deadline: PropTypes.string.isRequired,
   };
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours   = Math.floor((time/1000/60/60) % 24);
+    const days    = Math.floor((time/1000/60/60/24));
+    this.setState({ days, hours, minutes, seconds });
+  }
   
   render() {
     return (
