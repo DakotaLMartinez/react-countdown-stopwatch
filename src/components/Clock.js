c3e3765295ab71c76a6c1dca74856e7482c062e6
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-24 16:27:11 -0700

    adds 0 padding to numbers in countdown timer

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -22,6 +22,13 @@ class Clock extends Component {
     this.interval = setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
+  leading0(num) {
+    if (num < 10) {
+      return '0' + num;
+    }
+    return num;
+  }
+
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000) % 60);
@@ -34,10 +41,10 @@ class Clock extends Component {
   render() {
     return (
       <div>
-        <span className="mh1" id="Clock-days">{this.state.days} days</span>
-        <span className="mh1" id="Clock-hours">{this.state.hours} hours</span>
-        <span className="mh1" id="Clock-minutes">{this.state.minutes} minutes</span>
-        <span className="mh1" id="Clock-seconds">{this.state.seconds} seconds</span>
+        <span className="mh1" id="Clock-days">   {this.leading0(this.state.days)} days</span>
+        <span className="mh1" id="Clock-hours">  {this.leading0(this.state.hours)} hours</span>
+        <span className="mh1" id="Clock-minutes">{this.leading0(this.state.minutes)} minutes</span>
+        <span className="mh1" id="Clock-seconds">{this.leading0(this.state.seconds)} seconds</span>
       </div>
     );
   }
