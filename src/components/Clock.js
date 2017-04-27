a1a2eadb16d0e8bf7192c87de01537546c5a6d17
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-04-27 14:51:17 -0700

    installs prop-types package instead of accessing from React

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Clock extends Component {
   // add a constructor for state using 'reactstate'
